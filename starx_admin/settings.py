4d2b875d10b45c71e1fbb302833519b4deb83c3f
Ivica Ceraj <iceraj@gmail.com>
2014-01-28 14:21:43 -0500

    Adding dj-static

--- starx_admin/settings.py
+++ starx_admin/settings.py
@@ -84,6 +84,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = 'staticfiles'
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
 
 INSTALLED_APPS += ( 'dashboard', )
 
