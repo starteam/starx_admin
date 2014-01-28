4e14f0265b70f444a2d02932df153c33ff13d7a5
Ivica Ceraj <iceraj@gmail.com>
2014-01-28 14:17:45 -0500

    Adding /dashboard/ page

--- starx_admin/settings.py
+++ starx_admin/settings.py
@@ -84,3 +84,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.6/howto/static-files/
 
 STATIC_URL = '/static/'
+
+INSTALLED_APPS += ( 'dashboard', )
+
+# SiteID
+SITE_ID = 1
+
+# List of callables that know how to import templates from various sources.
+TEMPLATE_LOADERS = (
+    'django.template.loaders.filesystem.Loader',
+    'django.template.loaders.app_directories.Loader',
+)
