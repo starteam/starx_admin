b9439db4827cf8672d69df639a1d2431c0fb3092
Ivica Ceraj <iceraj@gmail.com>
2014-04-03 15:34:35 -0400

    Add user login

--- mit_accounts/views.py
+++ mit_accounts/views.py
@@ -1,8 +1,23 @@
 from django.shortcuts import render,render_to_response
 from django.template import RequestContext
+from django.contrib.auth.models import User
+    from django.contrib.auth import login
 
 # Create your views here.
 def index(request):
-    import pudb
-    pudb.set_trace()
+    email = request.environ['HTTP_SSL_EMAIL']
+    try:
+        user = User.objects.get(email=email)
+        if user.is_active:
+            # if user active make it authenticated
+            login(request, user)
+            print "User logged in"
+            print user
+        else:
+            pass
+    except User.DoesNotExist:
+        print "User does not exist"
+        print user
+        pass
+
     return render_to_response("mit_accounts/index.html",  RequestContext(request))
